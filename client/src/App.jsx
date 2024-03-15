2cfc229da03130ff58772e96a266a2fd1acbecef
gokulnathpeace@gmail.com <gokulnathpeace@gmail.com>
2024-03-15 09:08:18 +0530

    feat: Added light and dark colors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 function App() {
   return (
     <>
-      <p className="h-full text-center text-3xl text-white m-8 font-bold">
+      <p className="h-full text-center text-3xl text-gray-900 m-8 font-bold">
         MERN APP TEMPLATE
       </p>
+      <button className="btn bg-dark-100 text-light-100 shadow-light-100">
+        Button
+      </button>
     </>
   );
 }
